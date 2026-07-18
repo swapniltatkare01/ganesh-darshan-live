680fb9731884e52b1c2bf5f9a1f6a5fcd3f9972a
Swapnil Tatkare <swapnil.tatkare25@spit.ac.in>
2026-07-18 15:36:41 +0530

    updated mandal card generation

--- app/components/MandalCard.js
+++ app/components/MandalCard.js
@@ -1,7 +1,8 @@
 import Button from "./Button";
 export default function MandalCard(props){
     return(
-        <div className="flex flex-col gap-4 border-2 border-gray-300 rounded-lg p-4 ">
+        <div className="flex flex-col gap-4   rounded-lg p-4 shadow-lg ">
+            <div></div>
             <h1 className="text-xl font-bold">{props.name}</h1>
             <p className="text-gray-600" >📍{props.location}</p>
             <Button text="Watch Live"/>
--- app/page.js
+++ app/page.js
@@ -3,25 +3,44 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import MandalCard from "./components/MandalCard";
 export default function Home() {
+  const mandals = [
+    {
+      name: "Lalbaugcha Raja",
+      location: "Lalbaug, Mumbai"
+    },
+
+
+
+    {
+      name: "GSB Seva Mandal",
+      location: "King's Circle, Mumbai"
+    },
+
+
+    {
+      name: "Andhericha Raja",
+      location: "Andheri, Mumbai"
+    }
+  ];
+
   return (
 
     <div >
       <Navbar />
       <Hero />
-      <MandalCard
-  name="Lalbaugcha Raja"
-  location="Lalbaug, Mumbai"
-/>
+      <div className="grid grid-cols-3 gap-6">
+        {mandals.map((mandal) => {
+          return (
+            <MandalCard
+              key={mandal.name}
+              name={mandal.name}
+              location={mandal.location}
+            />
+          );
+        })}
+      </div>
 
-<MandalCard
-  name="GSB Seva Mandal"
-  location="King's Circle, Mumbai"
-/>
 
-<MandalCard
-  name="Andhericha Raja"
-  location="Andheri, Mumbai"
-/>
 
     </div>
   );
